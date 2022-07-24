91a5ac718a1e5d52d4c6656ae965f49d462d8b05
KSait0 <6wayofpain@gmail.com>
2022-07-24 19:19:47 +0300

    Черновик

--- js/big-picture-render.js
+++ js/big-picture-render.js
@@ -2,7 +2,8 @@ import {isEscapeKey} from './util.js';
 
 const bigPicture = document.querySelector('.big-picture');
 const socialCommentCount = document.querySelector('.social__comment-count');
-let visibleComments = bigPicture.querySelector('.social__comments');
+const visibleComments = bigPicture.querySelector('.social__comments');
+const commentsCount =  bigPicture.querySelector('.comments-count');
 const commentsLoaderButton = document.querySelector('.comments-loader');
 const bigPictureCloseButton = bigPicture.querySelector('.big-picture__cancel');
 const picturesList = document.querySelector('.pictures');
@@ -24,12 +25,11 @@ function bigPictureClose() {
 
 function bigPictureOpen() {
   bigPicture.classList.remove('hidden');
-  // socialCommentCount.classList.add('hidden');
-  // commentsLoaderButton.classList.add('hidden');
   document.body.classList.add('modal-open');
 
   bigPictureCloseButton.addEventListener('click', bigPictureClose);
   document.addEventListener('keydown', onBigPictureEscKeydown);
+  // commentsLoaderButton.addEventListener('click', loadMoreComments);
 }
 
 function getSingleComment({ avatar, message, name }) {
@@ -59,25 +59,37 @@ function createSocialComments(comments) {
 function bigPictureRender({ url, likes, comments, description }) {
   bigPicture.querySelector('.big-picture__img img').src = url;
   bigPicture.querySelector('.likes-count').textContent = likes;
-  bigPicture.querySelector('.comments-count').textContent = comments.length;
+  commentsCount.textContent = comments.length;
   bigPicture.querySelector('.social__comments').textContent = '';
   bigPicture.querySelector('.social__caption').textContent = description;
   visibleComments.appendChild(createSocialComments(comments.slice(0, 5)));
   bigPictureOpen();
-  // if (comments.length % 5 === 0) {
-  //   commentsLoaderButton.classList.add('hidden');
-  // }
 }
 
-function onCommentsLoaderButtonClick(comments) {
-  // const allComments = comments.slice();
-  // visibleComments.innerHTML = '';
+function loadMoreComments(comments) {
+  // const allCommentsList = comments.slice();
   const lastVisibleCommentsIndex = visibleComments.length - 1;
-  visibleComments = comments.slice(lastVisibleCommentsIndex, (lastVisibleCommentsIndex + 5));
-  return visibleComments;
+  if (comments.length <= 5) {
+    commentsLoaderButton.classList.add('hidden');
+
+    commentsLoaderButton.removeEventListener('click', loadMoreComments);
+  } else {
+    visibleComments.appendChild(createSocialComments(comments.slice(0, (lastVisibleCommentsIndex + 5))));
+
+    commentsLoaderButton.addEventListener('click', loadMoreComments);
+  }
 }
 
-commentsLoaderButton.addEventListener('click', onCommentsLoaderButtonClick);
+// function onCommentsLoaderButtonClick() {
+//   if (comments.length <= 5) {
+//     commentsLoaderButton.classList.add('hidden');
+//     commentsLoaderButton.removeEventListener('click', loadMoreComments);
+//   } else {
+
+//   }
+// }
+
+// commentsLoaderButton.addEventListener('click', loadMoreComments);
 
 function initBigPicture(allImages) {
   picturesList.addEventListener('click', (evt) => {
@@ -86,7 +98,12 @@ function initBigPicture(allImages) {
       const photo = allImages.find((item) => item.id === parseInt(id, 10));
       bigPictureRender(photo);
     }
+    loadMoreComments(allImages);
   });
 }
 
 export {initBigPicture};
+
+// Покажите блоки счётчика комментариев .social__comment-count и загрузки новых комментариев .comments-loader, убрав у них класс hidden.
+
+// В модуле, который отвечает за отрисовку окна с полноразмерным изображением, доработайте код по выводу списка комментариев таким образом, чтобы список показывался не полностью, а по 5 элементов, и следующие 5 элементов добавлялись бы по нажатию на кнопку «Загрузить ещё». Не забудьте реализовать обновление числа показанных комментариев в блоке .social__comment-count.
